5652105e26acd7a8338ab99051ed040bbb7e5802
tesla0583 <firdavskhodjaev@gmail.com>
2021-07-15 11:34:43 +0500

    finished middleware chapter

---
+++ app/Http/Middleware/EnsureTokenIsValid.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class EnsureTokenIsValid
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->input('token') !== '12345') {
+            return redirect('home');
+        }
+
+        return $next($request);
+    }
+}
